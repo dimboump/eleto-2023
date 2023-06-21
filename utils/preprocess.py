6dbce8ad9c5f7e4d59693d710931befff81eba0a
dimboump <dimboump@pm.me>
2023-06-21 12:28:10 +0300

    Apply PEP8 styles

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 import re
 
 import pandas as pd
@@ -7,20 +8,21 @@ import pandas as pd
 class TextPreprocessor:
     """Preprocess text by removing cross references and punctuation,
     and replacing numbers with zeros."""
+
     def __init__(
-        self, 
-        text: str, 
-        remove_crossrefs: bool = True, 
-        digits_to_zeros: bool = True, 
+        self,
+        text: str,
+        remove_crossrefs: bool = True,
+        digits_to_zeros: bool = True,
         remove_punctuation: bool = True
     ):
         self.text = text
         self.remove_crossrefs = remove_crossrefs
         self.digits_to_zeros = digits_to_zeros
-        self.remove_punctuation = remove_punctuation
+        self.remove_punct = remove_punctuation
 
     def _remove_crossrefs(self) -> None:
-        """Remove cross references from text. 
+        """Remove cross references from text.
         A cross reference is a string of the form (ΑΚ 123) or (BGB § 123)."""
         pattern = r'\((ΑΚ|AK|BGB)[\s§]*\d+\)'
         self.text = re.sub(pattern, '', self.text)
@@ -35,16 +37,20 @@ class TextPreprocessor:
         self.text = re.sub(r'[^\w\s]', '', self.text)
 
     def preprocess(self) -> str:
-        """Preprocess text by removing cross references and punctuation, 
+        """Preprocess text by removing cross references and punctuation,
         and replacing numbers with zeros."""
-        if self.remove_crossrefs: self._remove_crossrefs()
-        if self.digits_to_zeros: self._digits_to_zeros()
-        if self.remove_punctuation: self._remove_punctuation()
+        if self.remove_crossrefs:
+            self._remove_crossrefs()
+        if self.digits_to_zeros:
+            self._digits_to_zeros()
+        if self.remove_punct:
+            self._remove_punctuation()
         return self.text
 
 
 class DataFramePreprocessor:
     """Preprocess a dataframe by adding a column with the preprocessed text."""
+
     def __init__(
         self,
         df: pd.DataFrame,
@@ -55,15 +61,14 @@ class DataFramePreprocessor:
         self.df = df
         self.remove_crossrefs = remove_crossrefs
         self.digits_to_zeros = digits_to_zeros
-        self.remove_punctuation = remove_punctuation
-    
-    def preprocess(self):
+        self.remove_punct = remove_punctuation
+
+    def preprocess(self) -> pd.DataFrame:
         preprocessed = (self.df['text']
                         .apply(lambda x: TextPreprocessor(x,
                                                           self.remove_crossrefs,
                                                           self.digits_to_zeros,
-                                                          self.remove_punctuation
-                                                         ).preprocess()))
-        print(preprocessed)
+                                                          self.remove_punct
+                                                          ).preprocess()))
         preprocessed_df = pd.DataFrame({'preprocessed': preprocessed})
         return pd.concat([self.df, preprocessed_df], axis=1)
--- utils/unique_id.py
+++ utils/unique_id.py
@@ -2,10 +2,13 @@ import csv
 import os
 import random
 import sys
-from string import ascii_lowercase, digits
-from typing import Dict, List
+from string import ascii_lowercase
+from string import digits
+from typing import Dict
+from typing import List
 
 Row = Dict[str, str]
+
 
 def generate_unique_id(id: int, zfill: int = 0) -> str:
     random_str = ''.join(random.choices(ascii_lowercase + digits, k=6))
@@ -13,19 +16,23 @@ def generate_unique_id(id: int, zfill: int = 0) -> str:
     unique_id = f"{id_leading_zeros}-{random_str}"
     return unique_id
 
+
 def read_csv(file: str, delim: str) -> List[Row]:
     with open(file, 'r', encoding='utf-8') as f_in:
         reader = csv.DictReader(f_in, delimiter=delim)
         rows = [row for row in reader]
     return rows
 
+
 def count_unique_ids(rows: List[Row]) -> int:
     n_unique_ids = len(set([row['id'] for row in rows]))
     return n_unique_ids
 
+
 def get_zfill(n: int) -> int:
     zfill = len(str(n)) if len(str(n)) > 2 else 2
     return zfill
+
 
 def add_unique_ids(rows: List[Row], zfill: int) -> List[Row]:
     for row in rows:
@@ -33,9 +40,10 @@ def add_unique_ids(rows: List[Row], zfill: int) -> List[Row]:
         print(row['uid'])
     return rows
 
+
 def write_csv(rows: List[Row], out_file: str, delim: str) -> None:
     with open(out_file, 'w', encoding='utf-8', newline='') as f_out:
-        fields = ['id', 'uid'] + [k for k in rows[0].keys() 
+        fields = ['id', 'uid'] + [k for k in rows[0].keys()
                                   if k not in {'id', 'uid'}]
         writer = csv.DictWriter(f_out, fieldnames=fields, delimiter=delim)
         writer.writeheader()
@@ -43,7 +51,7 @@ def write_csv(rows: List[Row], out_file: str, delim: str) -> None:
 
 
 def main() -> int:
-    ret = 0
+    retv = 0
     file = sys.argv[1]
     file_no_ext, file_ext = file.rsplit('.', 1)
 
@@ -65,9 +73,10 @@ def main() -> int:
         write_csv(rows, out_file, delim)
     except IOError as e:
         print(f"Error writing to file: {e}")
-        ret |= 1
+        retv |= 1
 
-    return ret
+    return retv
+
 
 if __name__ == '__main__':
     raise SystemExit(main())
